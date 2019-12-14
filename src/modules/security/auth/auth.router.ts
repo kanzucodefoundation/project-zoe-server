0d3bced3ca8c0435ac5f6ec59038b65d4cc6089a
Timothy Emmanuel Kasasa <timothyk@laboremus.no>
2019-12-14 11:54:22 +0300

    Completed Authentication module

--- src/modules/security/auth/auth.router.ts
+++ src/modules/security/auth/auth.router.ts
@@ -1,31 +1,31 @@
 import {Request, Response, Router} from "express";
 import passport from 'passport'
 import {createJWT} from "./jwtConstants";
+import {authorize} from "../../../utils/middleware";
 
 const router = Router();
 
-
-router.post('/login', function(req: Request, res: Response) {
+router.post('/login', function (req: Request, res: Response) {
     passport.authenticate(
         'local',
         {session: false},
         function (error, user, info) {
             if (error) {
-                console.log('Auth Error',error)
+                console.log('Auth Error', error)
                 res.status(401).json({message: error});
             } else if (!user) {
                 res.status(401).json({message: 'invalid user', info});
             } else {
-                const payload: any = {
-                    username: user.username,
-                    contactId: user.contactId
-                };
-                const token = createJWT(payload);
+                const token = createJWT(user);
                 res.status(200)
                     .send({user, token});
             }
         }
     )(req, res);
+});
+
+router.get('/profile', authorize, function (req: Request, res: Response) {
+    res.json(req.user)
 });
 
 export default router;
--- src/modules/security/usergroup/usergroup.service.ts
+++ src/modules/security/usergroup/usergroup.service.ts
@@ -1,8 +1,14 @@
-import UserGroupModel, {IUserGroup} from './usergroup.model'
+import UserGroupModel, {IUserGroup, UserGroupDto} from './usergroup.model'
+import * as repo from "../../../utils/repository";
 
 
 export const exitsAsync = async (name: string): Promise<boolean> => {
     return await UserGroupModel.exists({name})
+};
+
+export const createAsync = async (data: any): Promise<IUserGroup> => {
+    const dt = UserGroupDto.create(data);
+    return await repo.createAsync<IUserGroup>(UserGroupModel, dt)
 };
 
 export const getByNameAsync = async (name: string): Promise<IUserGroup> => {
