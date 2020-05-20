dcdbf02d4bc3c49831c911996b6b0803b256ac10
ibia-byte <retyibea@yahoo.com>
2020-05-20 15:08:33 +0300

    made changes in task API

--- src/tasks/tasks/tasks.controller.ts
+++ src/tasks/tasks/tasks.controller.ts
@@ -22,7 +22,7 @@ export class TasksController {
     }
 
     @Put(':id')
-    update(@Body() updateTaskDto: CreateTaskDto, @Param('id') id):string{
+    update(@Body() updateTaskDto: CreateTaskDto, @Param('Ministry') id):string{
         return `Update ${id} - Ministry: ${updateTaskDto.ministry}`;
     }
     
