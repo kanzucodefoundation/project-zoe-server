4f3433dab2d06d3b1873ad05897f527cfeb117ce
kiken-eng <kanyecope@gmail.com>
2021-08-06 10:21:15 +0300

    Added the sending of an email to a member once there request is approved

--- src/groups/services/group-membership-request.service.ts
+++ src/groups/services/group-membership-request.service.ts
@@ -6,10 +6,7 @@ import GroupMembershipRequestSearchDto from '../dto/membershipRequest/search-req
 import GroupMembershipRequest from '../entities/groupMembershipRequest.entity';
 import { hasValue } from 'src/utils/validation';
 import GroupMembershipRequestDto from '../dto/membershipRequest/group-membership-request.dto';
-import {
-  NewMcDto,
-  NewRequestDto,
-} from '../dto/membershipRequest/new-request.dto';
+import { NewRequestDto } from '../dto/membershipRequest/new-request.dto';
 import { getPersonFullName } from 'src/crm/crm.helpers';
 import { ContactsService } from 'src/crm/contacts.service';
 import Contact from 'src/crm/entities/contact.entity';
@@ -77,6 +74,16 @@ export class GroupMembershipRequestService {
       relations: ['person'],
     });
 
+    //Checking if a member is already attached to an MC
+    const mcAttachedTo = await this.groupRepository.findOne({
+      where: { id: data.churchLocation },
+    });
+
+    const categoryID = mcAttachedTo.categoryId;
+    if (categoryID.toLocaleLowerCase() === 'mc') {
+      throw new HttpException('User is already registered to an MC', 400);
+    }
+
     const isPendingRequest = await this.repository.count({
       where: { contactId: data.contactId },
     });
@@ -125,98 +132,6 @@ export class GroupMembershipRequestService {
         relations: ['contact', 'contact.person', 'group'],
       })
     ).map(this.toDto);
-  }
-
-  //Joining an MC
-  async mcRequest(data: NewMcDto): Promise<GroupMembershipRequestDto | any> {
-    try {
-      const checkEmailExist = await this.emailRepository.findOne({
-        where: [{ value: data.email }],
-      });
-
-      //if email given exists
-      if (checkEmailExist) {
-        const personId = checkEmailExist.contactId;
-
-        //Find groupId attached to the user
-        const churchGroupId = await this.membershipRepository.find({
-          where: { contactId: personId },
-          select: ['groupId'],
-        });
-
-        //Getting the church location id
-        const churchLocationById = churchGroupId[0].groupId;
-
-        //get closest mc
-        const groupDetails = {
-          placeId: data.residencePlaceId,
-          parentGroupId: churchLocationById,
-        };
-
-        //Checking if a member is already attached to an MC
-        const mcAttachedTo = await this.groupRepository.findOne({
-          where: { id: churchLocationById },
-        });
-
-        const categoryID = mcAttachedTo.categoryId;
-        if (categoryID.toLocaleLowerCase() === 'mc') {
-          throw new HttpException('User is already registered to an MC', 400);
-        }
-
-        //Validate Request before submission
-        const isPendingRequest = await this.repository.count({
-          where: { contactId: personId },
-        });
-
-        if (isPendingRequest > 0) {
-          throw new HttpException('User already has a pending request', 400);
-        } else {
-          const info = await this.contactService.getClosestGroups(groupDetails);
-          const result = await this.repository
-            .createQueryBuilder()
-            .insert()
-            .values({
-              contactId: personId,
-              parentId: 1,
-              groupId: info.groupId,
-              distanceKm: Math.round(info.distance / 1000),
-            })
-            .execute();
-
-          //Return person details and category.
-          const user = await this.contactRepository.findOne(personId, {
-            relations: ['person'],
-          });
-
-          const closestCellData = JSON.parse(info.groupMeta);
-          console.log(closestCellData);
-          const mailerData: IEmail = {
-            to: `${closestCellData.email}`,
-            subject: 'Join MC Request',
-            html: `
-          <h3>Hello ${closestCellData.leaders},</h3></br>
-          <h4>I hope all is well on your end.<h4></br>
-          <p>${user.person.firstName} ${user.person.lastName} who lives in ${data.residenceDescription},
-          would like to join your Missional Community ${info.groupName}.</br>
-          You can reach ${user.person.firstName} on ${data.phone} or ${data.email}.</p></br>
-          <p>Cheers!</p>
-          `,
-          };
-          await sendEmail(mailerData);
-
-          return (
-            await this.repository.find({
-              where: { contactId: personId },
-              relations: ['contact', 'contact.person', 'group'],
-            })
-          ).map(this.toDto);
-        }
-      } else {
-        throw new HttpException('Email is not registered', 400);
-      }
-    } catch (error) {
-      console.log(error);
-    }
   }
 
   async update(): Promise<any> {
