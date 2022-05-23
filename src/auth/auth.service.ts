2bf96ed5943758b587b8d044966fc03592979c70
Peter Kakoma <kakoma@kanzucode.com>
2022-05-24 01:02:36 +0300

    Add tenancy to generated JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,18 @@
-import { HttpException, Injectable, Logger } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
-import { cleanUpUser, createUserDto } from './auth.helpers';
-import { UserDto } from './dto/user.dto';
-import { IEmail, sendEmail } from 'src/utils/mailerTest';
-import { ForgotPasswordResponseDto } from './dto/forgot-password-response.dto';
-import { ResetPasswordResponseDto } from './dto/reset-password-response.dto';
-import { UpdateUserDto } from '../users/dto/update-user.dto';
-import { JwtHelperService } from './jwt-helpers.service';
-import { UserListDto } from 'src/users/dto/user-list.dto';
-import Roles from 'src/users/entities/roles.entity';
-import { In, Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { JwtService } from '@nestjs/jwt';
-import { LoginResponseDto } from './dto/login-response.dto';
+import { HttpException, Injectable, Logger } from "@nestjs/common";
+import { UsersService } from "../users/users.service";
+import { cleanUpUser, createUserDto } from "./auth.helpers";
+import { UserDto } from "./dto/user.dto";
+import { IEmail, sendEmail } from "src/utils/mailerTest";
+import { ForgotPasswordResponseDto } from "./dto/forgot-password-response.dto";
+import { ResetPasswordResponseDto } from "./dto/reset-password-response.dto";
+import { UpdateUserDto } from "../users/dto/update-user.dto";
+import { JwtHelperService } from "./jwt-helpers.service";
+import { UserListDto } from "src/users/dto/user-list.dto";
+import Roles from "src/users/entities/roles.entity";
+import { In, Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { JwtService } from "@nestjs/jwt";
+import { LoginResponseDto } from "./dto/login-response.dto";
 
 @Injectable()
 export class AuthService {
@@ -27,17 +27,17 @@ export class AuthService {
   async validateUser(username: string, pass: string): Promise<UserDto | null> {
     const user = await this.usersService.findByName(username);
     if (!user) {
-      Logger.warn('invalid username: ', username);
+      Logger.warn("invalid username: ", username);
       return null;
     }
-    console.log('Check if user is active', user.isActive);
+    console.log("Check if user is active", user.isActive);
     if (!user.isActive) {
-      Logger.warn('User Inactive', username);
+      Logger.warn("User Inactive", username);
       return null;
     }
     const roles = [];
     user.userRoles.forEach((it) => roles.push(...it.roles.permissions));
-    console.log('Check if user is active', user.isActive);
+    console.log("Check if user is active", user.isActive);
     const valid = await user.validatePassword(pass);
     if (valid) {
       cleanUpUser(user);
@@ -45,15 +45,23 @@ export class AuthService {
       dto.permissions = roles;
       return dto;
     } else {
-      Logger.warn('invalid password: ', username);
+      Logger.warn("invalid password: ", username);
       return null;
     }
   }
 
-  async generateToken(user: UserDto | UserListDto): Promise<LoginResponseDto> {
+  async generateToken(
+    user: UserDto | UserListDto,
+    tenant: string,
+  ): Promise<LoginResponseDto> {
     const userPermissions = await this.getPermissions(user.roles);
     user.permissions = userPermissions;
-    const payload = { ...user, sub: user.id, permissions: userPermissions };
+    const payload = {
+      ...user,
+      sub: user.id,
+      aud: tenant,
+      permissions: userPermissions,
+    };
     const token = await this.jwtService.signAsync(payload);
     return { token, user };
   }
@@ -66,7 +74,7 @@ export class AuthService {
   async forgotPassword(username: string): Promise<ForgotPasswordResponseDto> {
     const userExists = await this.usersService.findByName(username);
     if (!userExists) {
-      throw new HttpException('User Not Found', 404);
+      throw new HttpException("User Not Found", 404);
     }
 
     const user = await this.usersService.findOne(userExists.id);
@@ -75,7 +83,7 @@ export class AuthService {
 
     const mailerData: IEmail = {
       to: `${(await user).username}`,
-      subject: 'Reset Password',
+      subject: "Reset Password",
       html: `
             <h3>Hello ${user.fullName}</h3></br>
             <h4>Here is a link to reset your Password!<h4></br>
@@ -93,7 +101,7 @@ export class AuthService {
   ): Promise<ResetPasswordResponseDto> {
     const decodedToken = await this.jwtHelperService.decodeToken(token);
     if (!decodedToken) {
-      throw new HttpException('Incorrect Token, User not retrieved', 404);
+      throw new HttpException("Incorrect Token, User not retrieved", 404);
     }
     const userFromDb = await this.usersService.findOne(decodedToken.id);
 
@@ -105,11 +113,11 @@ export class AuthService {
     };
     const user = await this.usersService.update(data);
     if (!user) {
-      throw new HttpException('User Password Not Updated', 404);
+      throw new HttpException("User Password Not Updated", 404);
     }
     const mailerData: IEmail = {
       to: `${(await user).username}`,
-      subject: 'Password Change Confirmation',
+      subject: "Password Change Confirmation",
       html: `
           <h3>Hello ${(await user).fullName},</h3></br>
           <h4>Your Password has been changed successfully!<h4></br>
@@ -117,10 +125,10 @@ export class AuthService {
     };
     const mailURL = await sendEmail(mailerData);
     if (mailURL) {
-      const message = 'Password Change Successful';
+      const message = "Password Change Successful";
       return { message, mailURL, user };
     } else {
-      Logger.error('Email not sent');
+      Logger.error("Email not sent");
     }
   }
 
@@ -128,7 +136,7 @@ export class AuthService {
     const permissions: string[] = [];
 
     const getPermissions = await this.rolesRepository.find({
-      select: ['permissions'],
+      select: ["permissions"],
       where: { role: In(roles), isActive: true },
     });
     getPermissions.map((it: any) => permissions.push(...it.permissions));
