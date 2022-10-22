5d97b2b7925a79718a0ec94359ad5e884600aaed
Peter Kakoma <kakoma@kanzucode.com>
2022-10-22 07:31:29 +0300

    Return initial service attendance reports

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Inject } from "@nestjs/common";
 import GroupEvent from "src/events/entities/event.entity";
 import { Connection, Repository, FindConditions } from "typeorm";
 import { UserDto } from "src/auth/dto/user.dto";
+import { EventCategories } from "src/events/enums/EventCategories";
+import { lowerCaseRemoveSpaces } from "src/utils/stringHelpers";
 
 @Injectable()
 export class ReportsService {
@@ -13,12 +15,43 @@ export class ReportsService {
 
   async getReport(name: string, user: UserDto): Promise<any> {
     const filter: FindConditions<GroupEvent> = {};
-
-    const reportData = await this.repository.find({
-      select: ["name", "categoryId", "metaData", "id"],
+    switch (name) {
+      case "service-attendance":
+        filter.categoryId = EventCategories.Garage;
+        break;
+      default:
+      // Nothing to see here
+    }
+    let reportResponse = {
+      metaData: {
+        name: name,
+        columns: [],
+      },
+      data: [],
+    };
+    const dbReports = await this.repository.find({
+      select: ["name", "categoryId", "metaData", "id", "startDate"],
       relations: ["category", "group", "group.members", "attendance"],
       where: filter,
     });
-    return "Placeholder text for now";
+    // Service Attendance report info
+    reportResponse.metaData.columns.push({
+      name: "location",
+      label: "Location",
+    });
+    for (const report of dbReports) {
+      let reportDateLabel = report.startDate.toISOString().split("T")[0];
+      let reportDate = reportDateLabel.replace(/-/g, ""); // Remove hyphens
+      let rowData = {
+        location: report.group.name,
+        [reportDate]: report.metaData.numberOfAdults,
+      };
+      reportResponse.data.push(rowData);
+      reportResponse.metaData.columns.push({
+        name: reportDate,
+        label: reportDateLabel,
+      });
+    }
+    return reportResponse;
   }
 }
