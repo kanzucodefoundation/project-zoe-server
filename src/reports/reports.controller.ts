6c2c16bb1a8ba5c66500bd5b82fa69890134373f
Peter Kakoma <kakoma@kanzucode.com>
2023-06-30 15:25:46 +0300

    Return MC report - WIP

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -88,4 +88,77 @@ export class ReportsController {
   ) {
     return this.reportService.getReportSubmission(reportId, submissionId);
   }
+
+  @Get(":reportId/weekly-mc-reports")
+  async getWeeklyMCReports(@Param("reportId") reportId: number): Promise<any> {
+    const submissions = await this.reportService.getReportSubmissions(reportId);
+
+    // Intermediate data structure to organize the data
+    const mcDataMap: Record<string, any> = {};
+
+    // Process submissions and organize the data by mcName and month-Sunday
+    submissions.data.forEach((submission: any) => {
+      const mcName = submission.mcName;
+      const mcMemberCount = submission.mcMemberCount;
+      const mcParticipantCount = parseInt(submission.mcParticipantCount);
+      const submittedAt = submission.submittedAt.toISOString();
+      const submittedDate = new Date(submittedAt);
+      const year = submittedDate.getUTCFullYear();
+      const month = submittedDate.getUTCMonth();
+      const day = submittedDate.getUTCDay();
+
+      // Find the next Sunday date after the submitted date
+      const sundayDate = new Date(submittedDate);
+      sundayDate.setDate(
+        submittedDate.getDate() + (7 - submittedDate.getDay()),
+      );
+
+      // Generate the key in the format "YYYYMMDD" for the Sunday date
+      const sundayKey = sundayDate.toISOString().slice(0, 10).replace(/-/g, "");
+
+      // Create the mcData object if it doesn't exist
+      if (!mcDataMap[mcName]) {
+        mcDataMap[mcName] = {
+          mcName,
+          mcMemberCount,
+        };
+      }
+
+      // Store the mcParticipantCount for the corresponding Sunday
+      mcDataMap[mcName][sundayKey] = mcParticipantCount;
+    });
+
+    // Calculate the average value for each mcData object
+    Object.values(mcDataMap).forEach((mcData: any) => {
+      const countValues = Object.values(mcData).slice(2);
+      if (countValues.length > 0) {
+        const sum: any = countValues.reduce(
+          (total: number, count: number) => total + count,
+          0,
+        );
+        const average: any = sum / countValues.length;
+        mcData.average = average.toFixed(2);
+      } else {
+        mcData.average = null; // or 0, depending on your preference
+      }
+    });
+
+    // Generate the final response in the desired format
+    const data = Object.values(mcDataMap);
+    const columns = [
+      { name: "mcName", label: "MC Name" },
+      { name: "mcMemberCount", label: "MC Members" },
+      ...submissions.data.map((submission: any) => ({
+        name: submission.submittedAt.toISOString(),
+        label: submission.submittedAt.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        }),
+      })),
+      { name: "average", label: "Average" },
+    ];
+
+    return { reportId, data, columns, footer: null };
+  }
 }
